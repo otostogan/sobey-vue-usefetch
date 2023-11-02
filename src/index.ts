a70cd952c735efdc015cbdc26c3ab0cd9ffff132
otostogan <otostogan2020@gmail.com>
2023-11-02 10:26:56 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+import { reactive, UnwrapRef, ref, onBeforeUnmount } from "vue";
+
+export enum RequestStatus {
+	INIT = "init",
+	LOADING = "loading",
+	FULFILLED = "fulfilled",
+	REJECTED = "rejected",
+}
+
+export interface State<T> {
+	data: T | null;
+	status: RequestStatus;
+	error: any | null;
+}
+
+type RequestFunction<T, A> = (args: A) => Promise<T>;
+
+export interface IOptions<T, A = void> {
+	request: RequestFunction<T, A>;
+	defaultData?: T;
+}
+
+export function useFetch<T, A = void>({
+	request,
+	defaultData,
+}: IOptions<T, A>): [
+	State<T>,
+	(args?: A, refetch?: number) => Promise<T>,
+	() => void
+] {
+	const interval = ref<number | null>(null);
+
+	const state = reactive<State<T>>({
+		data: defaultData ? defaultData : null,
+		status: RequestStatus.INIT,
+		error: null,
+	});
+
+	const useFetch = async (args?: A, refetch?: number) => {
+		const fetch = () => {
+			state.status = RequestStatus.LOADING;
+			return request(args!)
+				.then((response) => {
+					state.data = response as unknown as UnwrapRef<T>;
+					state.status = RequestStatus.FULFILLED;
+					return response;
+				})
+				.catch((error) => {
+					console.log(error);
+					state.error = error;
+					state.status = RequestStatus.REJECTED;
+					throw error;
+				});
+		};
+		if (refetch) {
+			interval.value = setInterval(() => {
+				fetch();
+			}, refetch);
+
+			return Promise.resolve(state.data as T);
+		} else {
+			return fetch();
+		}
+	};
+
+	const resetInterval = () => {
+		if (interval.value) {
+			clearInterval(interval.value);
+		}
+	};
+
+	onBeforeUnmount(() => {
+		resetInterval();
+	});
+
+	return [state as State<T>, useFetch, resetInterval];
+}
